816c4dae1a8277105c0dcf8458dbe8944b0f68d4
Joy Alvarez <alvjoy11@gmail.com>
2016-08-06 23:59:13 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+app.secret_key = 'very secret'
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def submit():
+	data = request.form
+	# name =  request.form['name']
+	# location = request.form['dojo']
+	# language = request.form['fave_lang']
+	# comments = request.form['comment']
+	print data
+	return render_template('show.html', joy=data)
+	# return redirect('/')
+	# redirects back to the '/' route
+
+app.run(debug=True)
